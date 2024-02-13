6c5b4e3e968e602429b22e43436cce33bae71b4f
Bakhtin <jurij.bakhtin@contractor.calleridreputation.com>
2024-02-13 14:15:08 +0700

    chore: repository interface for key-value modeule

--- internal/server/appmodules/key-value/ports/secondary/repository.go
+++ internal/server/appmodules/key-value/ports/secondary/repository.go
@@ -7,10 +7,23 @@ import (
 	"github.com/jbakhtin/goph-keeper/internal/server/appmodules/key-value/domain/models"
 )
 
+type QuerySpecification interface {
+	Query() string
+	Value() []any
+}
+
+type KeyValueQuerySpecification interface {
+	Limit(QuerySpecification, int) QuerySpecification
+	Where(QuerySpecification) QuerySpecification
+	Or(...QuerySpecification) QuerySpecification
+	And(...QuerySpecification) QuerySpecification
+	UserID(int) QuerySpecification
+}
+
 type KeyValueRepository interface {
-	SaveKeyValue(ctx context.Context, secret models.KeyValue) (*models.KeyValue, error)
-	GetKeyValue(ctx context.Context, UUID uuid.UUID) (*models.KeyValue, error)
-	UpdateKeyValue(ctx context.Context, secret models.KeyValue) (*models.KeyValue, error)
-	DeleteKeyValue(ctx context.Context, secret models.KeyValue) (*models.KeyValue, error)
-	SearchKeyValue(ctx context.Context) ([]models.KeyValue, error) // ToDo: need add specifications
+	Create(context.Context, models.KeyValue) (*models.KeyValue, error)
+	Get(context.Context, uuid.UUID) (*models.KeyValue, error)
+	Update(context.Context, models.KeyValue) (*models.KeyValue, error)
+	Delete(context.Context, models.KeyValue) (*models.KeyValue, error)
+	Search(context.Context, QuerySpecification) ([]*models.KeyValue, error)
 }
